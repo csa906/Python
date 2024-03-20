bbc50ede2163c1a60fe3b842c5d4b046eb83fff2
csa906 <yourak1002@naver.com>
2024-03-20 16:21:19 +0900

    modified:   pygame_project/1_frame_background_stage_character.py 	new file:   pygame_project/test.py

--- pygame_project/1_frame_background_stage_character.py
+++ pygame_project/1_frame_background_stage_character.py
@@ -28,25 +28,13 @@ stage = pygame.image.load(os.path.join(image_path, "stage.png"))
 stage_size = stage.get_rect().size
 stage_height = stage_size[1]
 
-# 폴더 주소 가져오기
-dir = os.getcwd()
-
-# 배경 이미지 불러오기
-
-# 캐릭터(스프라이트) 불러오기
-
-# 이동할 좌표
-
-# 적 캐릭터
-
-# 폰트 정의 
-game_font = pygame.font.Font(None, 40) # 폰트 객체 생성 (폰트, 크기)
-
-# 총 시간
-total_time = 10
-
-# 시작 시간 
-start_ticks = pygame.time.get_ticks()
+# 캐릭터 만들기
+character = pygame.image.load(os.path.join(image_path, "character.png")) 
+character_size = character.get_rect().size
+character_width = character_size[0]
+character_height = character_size[1]
+character_x_pos = (screen_width - character_width) / 2
+character_y_pos = screen_height - character_height - stage_height
 
 # 이벤트 루프
 running = True
@@ -61,6 +49,7 @@ while(running):
 
     screen.blit(background, (0, 0))
     screen.blit(stage, (0, screen_height - stage_height))
+    screen.blit(character, (character_x_pos, character_y_pos))
 
     pygame.display.update()
 
