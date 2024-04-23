6af1b1b3d42fb8f0d78b6a5e10ab2636cf42e0ff
csa906 <81524983+csa906@users.noreply.github.com>
2024-04-23 21:38:55 +0900

    Delete python1 directory

--- python1/practice.py
+++
@@ -1,8 +0,0 @@
-def is_palindrome(word):
-    if(word == ''.join(reversed(word))):
-        return 1
-    elif(word != ''.join(reversed(word))):
-        return 0
-
-word = input()
-print(is_palindrome(word))
